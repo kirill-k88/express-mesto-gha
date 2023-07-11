323ef23b21c3c6ebcc7c1b2b4baf7c98a06067e8
kirill-k88 <kirill-k88@ya.ru>
2023-07-11 21:46:10 +0300

    fis single quot

--- controllers/handleError.js
+++ controllers/handleError.js
@@ -1,15 +1,15 @@
 const handleError = (err, res) => {
   if (err.name === 'ValidationError') {
     return res.status(400).send({
-      message: `Переданы некорректные данные`,
+      message: 'Переданы некорректные данные',
     });
   }
   if (err.name === 'CastError') {
     return res.status(404).send({
-      message: `Данных по указанному _id не найдено`,
+      message: 'Данных по указанному _id не найдено',
     });
   }
-  return res.status(500).send({ message: `Ошибка по умолчанию` });
+  return res.status(500).send({ message: 'Ошибка по умолчанию' });
 };
 
 class CastError extends Error {
