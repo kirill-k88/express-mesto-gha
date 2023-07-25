d110bc24a53a270e32d586c77b330f8e92983011
kirill-k88 <kirill-k88@ya.ru>
2023-07-25 08:55:58 +0300

    fix: error then email isn't unique

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,9 @@
 module.exports = (err, req, res, next) => {
+  if (err.message.includes('to be unique')) {
+    return res
+      .status(409)
+      .send({ message: 'Ползователь с таким email уже существует' });
+  }
   if (!err.statusCode) {
     return res.status(500).send({ message: 'На сервере произошла ошибка' });
   }
